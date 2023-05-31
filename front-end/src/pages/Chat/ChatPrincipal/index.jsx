fec5d75145104af61a5906467777e2f54d331af4
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-05-31 18:05:53 -0300

    append dinamico mensagens chat

--- front-end/src/pages/Chat/ChatPrincipal/index.jsx
+++ front-end/src/pages/Chat/ChatPrincipal/index.jsx
@@ -2,11 +2,40 @@ import "./style.css";
 
 import imagemPerfil from "../../../assets/images/logo.jpg";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState, useEffect } from "react";
+import jwt from 'jwt-decode' 
+
 
 export default function ChatPrincipal() {
 
+  const [ message, setMessage ] = useState("");
+  const [token, setToken] = useState('');
 
-  return (
+  useEffect(() => {
+    setToken(document.cookie.replace(/(?:(?:^|.*;\s*)jwt\s*\=\s*([^;]*).*$)|^.*$/, '$1'))
+  }, [])
+
+  const renderMessage = async (message) => {
+    const container = document.getElementsByClassName("conteudoChat")[0]
+    const novaDiv = document.createElement('div');
+    novaDiv.innerHTML = 
+    novaDiv.className = 'textoChatUser';
+    novaDiv.textContent = `${message.user.nome}: ${message.message}`;
+    container.appendChild(novaDiv);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const _message = {
+      user: jwt(token).secret,
+      message: message,
+      horario: new Date()
+    }
+    await renderMessage(_message)
+    setMessage("")
+  }
+
+  return token && (
     <div className="containerChat">
       <div className="chat-principal">
 
@@ -20,36 +49,29 @@ export default function ChatPrincipal() {
             <div id="searchIcon"><SearchIcon /></div>
           </div>
 
-          <div className="textoChatOutro">
-            Testando a digitação no chat
-            fasfafasf
-            fasfafasffasfasfs
-            fasfafasffasfasfsfasfsafsa
-            fasf
-            asf
-            asds
-            fasfafasffasfasfsfasfsafsafasfa
-          </div>
-
           <div className="conteudoChat">
-            <div className="textoChatUser">
-              Testando a digitação no chat
-              fasfafasf
-              fasfafasffasfasfs
-              fasfafasffasfasfsfasfsafsa
-              fasf
-              asf
-              asds
-              fasfafasffasfasfsfasfsafsafasfa
-            </div>
+
 
           </div>
 
         </div >
 
-        <div className="entradasChat">
-          <input id="campoDigitacao" type="text" placeholder="Mensagem" />
-        </div>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="entradasChat">
+            <input
+              id="campoDigitacao" 
+              type="text" 
+              placeholder="Mensagem"  
+              value={message}
+              required
+              maxLength='30'
+              onChange={e => setMessage(e.target.value)}
+              />
+            <button type="submit" className="sendMessage">
+              enviar
+            </button>
+          </div>
+        </form>
 
       </div>
     </div>
