68eb8f22e3e7759a961ca0b69f1c36ca33a18f65
Yasmim Rosa <mimsayrosa@gmail.com>
2023-06-10 20:43:05 -0300

    testes com Jest

--- back-end/src/test/controller.spec.js
+++ back-end/src/test/controller.spec.js
@@ -1,124 +1,97 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http")
-const server = require("../index")
-const expect = chai.expect;
-
-chai.use(chaiHttp);
+const request = require('supertest');
+const server = require('../index');
 
 let globalToken;
 
-describe("Testes das Rotas", () => {
-    before((done) => {
-        chai
-          .request(server)
-          .post("/login")
-          .send({ username: "sabrina_mattos@gmail.com", password: '123456' })
-          .end((err, res) => {
-            expect(err).to.be.null
-            expect(res).to.have.status(200)
-            expect(res).to.have.cookie('jwt')
-            
-            globalToken = res.header['set-cookie'][0].split('=')[1].split(';')[0]
-            done();
-          });
-      });
-    
-      after(function () {
-        // Feche o servidor após todos os testes
-        server.close()
-      });
+beforeAll((done) => {
+  request(server)
+    .post('/login')
+    .send({ username: 'sabrina_mattos@gmail.com', password: '123456' })
+    .end((err, res) => {
+      expect(err).toBeNull();
+      expect(res).toHaveProperty('status', 200);
+      expect(res).toHaveProperty('headers');
+      expect(res.headers).toHaveProperty('set-cookie');
+      const token = res.headers['set-cookie'][0].split('=')[1].split(';')[0];
+      globalToken = token;
+      done();
+    });
+});
 
-    // it("Deve criar um novo usuário", () =>{
-    //     chai
-    //     .request(server)
-    //     .post("/cadastro")
-    //     .send({ 
-    //       nome_completo: "Sabrina Mattos", 
-    //       curso: 1,
-    //       matricula: 1234567, 
-    //       email: "sabrina_mattos@gmail.com", 
-    //       celular: "619999699", 
-    //       password: 123456 })
-    //     .end((err, res) => {
-    //       expect(res).to.have.status(200)
-    //     })
-    // })
+afterAll((done) => {
+  // Feche o servidor após todos os testes
+  server.close(done);
+});
 
-    // it('Deve criar uma pergunta', (done) => {
-    //     chai
-    //         .request(server)
-    //         .post('/pergunta')
-    //         .send({
-    //             titulo: "Rei e Rainha da Derivada",
-    //             curso: 1,
-    //             conteudo: "É obrigatório participar do evento?",
-    //             filtro: "C1"
-    //         })
-    //         .set('Cookie', `jwt=${globalToken}`)
-    //         .end((err, res) => {
-    //             expect(res).to.have.status(201)
-    //             done()
-    //         })
-    // })
-
-    // it("Deve criar um aviso", (done) =>{
-    //     chai   
-    //         .request(server)
-    //         .post('/aviso/criar')
-    //         .send({
-    //             id_usuario: {
-    //               username: 'sabrina_mattos@gmail.com',
-    //               id: '6484d5f01035ba31b9d493d8',
-    //               nome: 'Sabrina Mattos',
-    //               curso: 1
-    //             },
-    //             tituloAviso: 'Monitoria de APC',
-    //             corpoAviso: 'A monitoria será na S10',
-    //             id_cursoAviso: 1,
-    //             filtro: 'APC'
-    //           })
-    //         .set("Authorization", `Bearer ${globalToken}`)
-    //         .end((err, res) => {
-    //             expect(res).to.have.status(201)
-    //             done()
-    //         })
-    // })
-
-    // it("Deve retornar um aviso existente", (done) => {
-    //     chai
-    //         .request(server)
-    //         .get("/aviso/6484d6820f85baedbbd2a2ae")
-    //         .set("Authorization", ` Bearer ${globalToken}`)
-    //         .end((err, res) => {
-    //             expect(res).to.have.status(201);
-    //             expect(res.body).to.be.an("object");
-    //             done();
-    //         });
-    // });
-
-
-    it("Deve permitir a edição de um aviso pelo proprietário", (done) => {
-        chai
-          .request(server)
-          .put(`/aviso/editar/6484d6820f85baedbbd2a2ae`)
-          .send({
-            titulo: "Editando",
-            conteudo: "Editado",
-            materia: "MDS"
-          })
-          .set("Authorization", `Bearer ${globalToken}`)
-          .end((err, res) => {
-            expect(res).to.have.status(200);
-            done();
-          });
+describe('Testes das Rotas', () => {
+  it('Deve criar um novo usuário', async () => {
+    const response = await request(server)
+      .post('/cadastro')
+      .send({
+        nome_completo: 'Yasmim Rosa',
+        curso: 1,
+        matricula: 1234567,
+        email: 'yasmim_rosa@gmail.com',
+        celular: '619999699',
+        password: 123456
       });
 
-    // it("Deve responder a uma pergunta", (done)=>{
-    //     chai
-    //         .request(server)
-    //         .post('/resposta')
-    // }) incompleto
+    expect(response).toHaveProperty('status', 200);
+  });
 
+  it('Deve criar uma pergunta', async () => {
+    const response = await request(server)
+      .post('/pergunta')
+      .send({
+        titulo: 'Rei e Rainha da Derivada',
+        curso: 1,
+        conteudo: 'É obrigatório participar do evento?',
+        filtro: 'C1'
+      })
+      .set('Cookie', `jwt=${globalToken}`);
 
+    expect(response).toHaveProperty('status', 201);
+  });
 
+  it('Deve criar um aviso', async () => {
+    const response = await request(server)
+      .post('/aviso/criar')
+      .send({
+        id_usuario: {
+          username: 'sabrina_mattos@gmail.com',
+          id: '6484d5f01035ba31b9d493d8',
+          nome: 'Sabrina Mattos',
+          curso: 1
+        },
+        tituloAviso: 'Monitoria de APC',
+        corpoAviso: 'A monitoria será na S10',
+        id_cursoAviso: 1,
+        filtro: 'APC'
+      })
+      .set('Authorization', `Bearer ${globalToken}`);
+
+    expect(response).toHaveProperty('status', 201);
+  });
+
+  it('Deve retornar um aviso existente', async () => {
+    const response = await request(server)
+      .get('/aviso/6484d6820f85baedbbd2a2ae')
+      .set('Authorization', `Bearer ${globalToken}`);
+
+    expect(response).toHaveProperty('status', 201);
+    expect(response.body).toBeInstanceOf(Object);
+  });
+
+  it('Deve permitir a edição de um aviso pelo proprietário', async () => {
+    const response = await request(server)
+      .put('/aviso/editar/6484d6820f85baedbbd2a2ae')
+      .send({
+        titulo: 'Editando',
+        conteudo: 'Editado',
+        materia: 'MDS'
+      })
+      .set('Authorization', `Bearer ${globalToken}`);
+
+    expect(response).toHaveProperty('status', 200);
+  });
 })
