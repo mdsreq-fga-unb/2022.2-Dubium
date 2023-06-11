b120e568ada6d9ea5a94bab2c3522c629dc0fced
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-11 16:15:10 -0300

    att chat instance

--- back-end/src/routes/usuario.js
+++ back-end/src/routes/usuario.js
@@ -11,4 +11,40 @@ router.get("/salvos/:id", passport.authenticate('jwt', { session: false }), usua
 router.get("/", passport.authenticate('jwt', { session: false }), usuarioController.obterUsuarios)
 router.post("/salvarFoto", passport.authenticate('jwt', { session: false }), usuarioController.salvarFoto)
 
+router.post("/chatInstance", passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { user, userTarget, privado } = req.body
+    const userIds = [user.id, userTarget.id]
+    const users = {user, userTarget}
+    const infosChat = {
+        usuarios: userIds,
+        privado: privado,
+        idChat: ''
+    }
+
+    new chatSchema({ privado: privado, usuarios: users }).save()
+        .then(data => {
+            infosChat.idChat = data._id
+            usuarioSchema.updateMany({ _id: { $in: userIds } }, { $push: { chats: infosChat } })
+            .then(() => {
+                res.status(200).send(data._id)
+            })
+            .catch(err => {
+                res.status(400).send({
+                    erro: "Falha ao salvar instância",
+                    message: err
+                })
+            })
+        })
+        .catch(err => {
+            res.status(400).send({
+                error: "Erro ao fazer requisição",
+                message: err
+            })
+        })
+
+    })
+
+
+
+
 module.exports = router;
