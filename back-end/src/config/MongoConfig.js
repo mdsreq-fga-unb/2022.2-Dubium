5e89cbf675a8b66c07876a7577e03d163d34449c
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-15 06:57:25 -0300

    first commit

--- back-end/src/config/MongoConfig.js
+++ back-end/src/config/MongoConfig.js
@@ -1,9 +1,10 @@
 const { default: mongoose } = require("mongoose")
+require('dotenv').config()
 
 
 // const connection = mongoose.connect("mongodb+srv://arthurmelo1918:71JuBrKKWQr6kAfU@dubium0.o4ru6rq.mongodb.net/Dubium")
 
-const connection = mongoose.connect("mongodb://0.0.0.0:27017/cadastrar-teste")
+const connection = mongoose.connect(process.env.DB_HOST)
     .then(() => {
         console.log("Mongoose rodando!")
     }) 
