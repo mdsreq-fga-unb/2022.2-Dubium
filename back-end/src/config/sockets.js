0a44a97ec8eca957c2b981819a0064b209f4c791
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-21 14:33:52 -0300

    .

--- back-end/src/config/sockets.js
+++ back-end/src/config/sockets.js
@@ -1,6 +1,6 @@
 const emitter = require('../auth/emitter.js');
 const http = require('../server.js')
-const io = require("socket.io")(http, {cors: {origin: "http://localhost:5173"}})
+const io = require("socket.io")(http, {cors: {origin: "https://dubium2.vercel.app"}})
 const chatService = require("../service/chatService.js")
 
 
